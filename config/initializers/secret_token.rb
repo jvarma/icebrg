f3b0549db3949cf56d9c907bb9827d6415736ff5
Jayant Varma <jayant@cellcast.in>
2012-03-12 16:02:55 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icebrg::Application.config.secret_token = '8a10c9a5a88b7b7f1fdf71a8256a7b0fe9a3bed01f36382a69d8160b776f3324e13f6412bb2c2d82a0e568bbeab9a0790298fa7b056acb41b3c0388039c96835'
